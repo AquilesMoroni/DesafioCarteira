bb9b7cfbcdb7f253a2abaf11cb6efe914432ba63
AquilesMoroni <125597723+AquilesMoroni@users.noreply.github.com>
2023-04-26 12:19:00 -0300

    new

--- CarteiraDigital/Startup.cs
+++ CarteiraDigital/Startup.cs
@@ -23,7 +23,7 @@ namespace CarteiraDigital
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddNHibernate(Configuration.GetConnectionString("AquilesConnection")); 
+            services.AddNHibernate(Configuration.GetConnectionString("DefaultConnection")); 
             services.AddControllersWithViews(); 
         }
 
